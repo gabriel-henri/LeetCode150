e19dd2205a6fc5302311e8ab0867a6928ab23507
gabriel-henri <gabrielhra00@gmail.com>
2024-11-04 17:38:14 -0300

    AdicionandoQuestão122

---
+++ 122-BestTimeToBuyAndSellStock2/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit_atual = 0;
+        int profit_total = 0;
+        int l = 0;
+        int r = 1;
+
+        while(r < prices.length){
+            if(prices[r] > prices[l] && prices[r] > prices[r-1]){
+                profit_atual = prices[r] - prices[l];
+            }
+            else{
+                profit_total += profit_atual;
+                profit_atual = 0;
+                l = r;
+            }
+            r++;
+        }
+
+        profit_total += profit_atual;
+
+        return profit_total;
+    }
+}
